0b31dff0e1ddff15603ac29414c9f6b87160e8a1
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-05-25 21:46:50 -0500

    más forma con grid y colores

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
     <div className="container">
       <nav>Navbar</nav>
       <main>Main</main>
-      <div>content1</div>
-      <div>content2</div>
-      <div>content3</div>
+      <div id="sidebar" >Sidebar</div>
+      <div id="content1" >content1</div>
+      <div id="content2" >content2</div>
+      <div id="content3" >content3</div>
       <footer>Footer</footer>
     </div>
   );
