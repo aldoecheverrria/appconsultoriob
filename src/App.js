a9181e0dcfa75ba55875e841c8744d2c85a2888c
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-05-25 21:35:10 -0500

    primer commit ya con elementos HTML integrados

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <nav>Navbar</nav>
+      <main>Main</main>
+      <div>content1</div>
+      <div>content2</div>
+      <div>content3</div>
+      <footer>Footer</footer>
     </div>
   );
 }
